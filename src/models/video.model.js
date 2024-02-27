3c48829524edc31c20410fcf35ac83636143d9d8
sandesh gaikwad <sandesh.gaikwad1911@gmail.com>
2024-02-27 08:54:28 +0530

    all other models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,42 +1,41 @@
-import mongoose from 'mongoose';
-import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new mongoose.Schema({
-
+const videoSchema = new Schema({
     videoFile: {
-        type: String, 
-        required: [true, "Please provide a file"]
+      type: String, //cloudinary url
+      required: true,
     },
     thumbnail: {
-        type: String, 
-        required: true
+      type: String, //cloudinary url
+      required: true,
     },
     title: {
-        type: String, 
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String, 
-        required: true
+      type: String,
+      required: true,
     },
     duration: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     views: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
-    isPublishe: {
-        type: Boolean,
-        default: true
+    isPublished: {
+      type: Boolean,
+      default: true,
     },
     owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-
-}, {timestamps: true});
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },{timestamps: true}
+);
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
